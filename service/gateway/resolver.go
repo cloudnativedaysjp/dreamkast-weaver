0cd376508ae7fdd36cd6809f9308c75b8f4117b7
ystkfujii <ystk.fujii0731@gmail.com>
2023-04-09 00:01:44 +0900

    feat: add service/gateway

--- service/gateway/resolver.go
+++ service/gateway/resolver.go
@@ -1,7 +1,11 @@
 package gateway
 
+import "dreamkast-weaver/service/cfpsvc"
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+type Resolver struct {
+	cfpSvc cfpsvc.T
+}
--- service/gateway/server.go
+++ service/gateway/server.go
@@ -1,28 +1,68 @@
 package gateway
 
 import (
-	"dreamkast-weaver/graph"
-	"log"
 	"net/http"
 	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/ServiceWeaver/weaver"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+
+	"dreamkast-weaver/graph"
+	"dreamkast-weaver/service/cfpsvc"
 )
 
 const defaultPort = "8080"
 
-func NewServer() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = defaultPort
+type Server struct {
+	handler  http.Handler
+	root     weaver.Instance
+	hostname string
+
+	cfpSvc cfpsvc.T
+}
+
+func NewServer(root weaver.Instance) (*Server, error) {
+	// Setup the services.
+	cfpSvc, err := weaver.Get[cfpsvc.T](root)
+	if err != nil {
+		return nil, err
 	}
+
+	hostname, err := os.Hostname()
+	if err != nil {
+		root.Logger().Debug(`cannot get hostname for frontend: using "unknown"`)
+		hostname = "unknown"
+	}
+
+	// Create the server.
+	s := &Server{
+		root:     root,
+		hostname: hostname,
+		cfpSvc:   cfpSvc,
+	}
+
+	r := http.NewServeMux()
 
 	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	r.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	r.Handle("/query", weaver.InstrumentHandler("query", srv))
 
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	// Set handler and return.
+	var handler http.Handler = r
+	handler = otelhttp.NewHandler(handler, "http") // add tracing
+	s.handler = handler
+
+	return s, nil
+}
+
+func (s *Server) Run(localAddr string) error {
+	lis, err := s.root.Listener("boutique", weaver.ListenerOptions{LocalAddress: localAddr})
+	if err != nil {
+		return err
+	}
+	s.root.Logger().Debug("Frontend available", "addr", lis)
+	return http.Serve(lis, s.handler)
 }
