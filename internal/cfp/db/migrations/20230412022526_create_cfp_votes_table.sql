0d567e75a427cde1e393cda81010ff1e2040e62c
Hiroki Okui <hiroki.okui@gmail.com>
2023-04-12 11:40:56 +0900

    feat: Add testing db and introduce dbmate

---
+++ internal/cfp/db/migrations/20230412022526_create_cfp_votes_table.sql
@@ -0,0 +1,10 @@
+-- migrate:up
+CREATE TABLE cfp_votes (
+  conference_name CHAR(36) NOT NULL,
+  talk_id INT NOT NULL,
+  created_at DATETIME NOT NULL,
+  global_ip CHAR(16)
+);
+
+-- migrate:down
+DROP TABLE cfp_votes
