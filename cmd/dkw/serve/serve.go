67a7c53a7b5e6d1813d6a44e90ca078b579fb31e
ystkfujii <ystk.fujii0731@gmail.com>
2023-05-07 20:25:32 +0900

    refactor: Move serve function to cmd from graph

--- cmd/dkw/serve/serve.go
+++ cmd/dkw/serve/serve.go
@@ -3,14 +3,32 @@ package serve
 import (
 	"context"
 	"log"
+	"net/http"
+	"time"
 
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/ServiceWeaver/weaver"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/spf13/cobra"
 
 	"dreamkast-weaver/internal/graph"
+	gm "dreamkast-weaver/internal/graph/middleware"
 )
 
-var Port string
+var (
+	Port string
+
+	corsOpts = cors.Options{
+		AllowedOrigins: []string{"https://*", "http://*"},
+		AllowedMethods: []string{"GET", "POST", "OPTIONS"},
+		AllowedHeaders: []string{
+			"Content-Type", "Accept", "Authorization",
+			"X-Amz-Date", "X-Api-Key", "X-Amz-Security-Token", "X-Amz-User-Agent",
+		},
+	}
+)
 
 // serveCmd represents the serve command.
 var Cmd = &cobra.Command{
@@ -18,9 +36,35 @@ var Cmd = &cobra.Command{
 	Short: "Run service",
 	Long:  "Run service",
 	Run: func(_ *cobra.Command, _ []string) {
-		graph.SetPort(Port)
-		if err := weaver.Run(context.Background(), graph.Serve); err != nil {
+		if err := weaver.Run(context.Background(), serve); err != nil {
 			log.Fatal(err)
 		}
 	},
+}
+
+func serve(ctx context.Context, r *graph.Resolver) error {
+	router := chi.NewRouter()
+	router.Use(gm.ClientIP)
+	router.Use(cors.Handler(corsOpts))
+
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{
+		Resolvers: r,
+	}))
+
+	opts := weaver.ListenerOptions{LocalAddress: ":" + Port}
+	lis, err := r.Listener("dkw-serve", opts)
+	if err != nil {
+		return err
+	}
+	log.Printf("Listener available on %v\n", lis)
+
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", Port)
+	s := http.Server{
+		ReadHeaderTimeout: 5 * time.Second,
+		Handler:           router,
+	}
+	return s.Serve(lis)
 }
--- internal/graph/resolver.go
+++ internal/graph/resolver.go
@@ -1,21 +1,10 @@
 package graph
 
 import (
-	"context"
-	"log"
-	"net/http"
-	"sync"
-	"time"
-
-	"github.com/99designs/gqlgen/graphql/handler"
-	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/ServiceWeaver/weaver"
-	"github.com/go-chi/chi"
-	"github.com/go-chi/cors"
 
 	"dreamkast-weaver/internal/cfp"
 	"dreamkast-weaver/internal/dkui"
-	gm "dreamkast-weaver/internal/graph/middleware"
 )
 
 // This file will not be regenerated automatically.
@@ -24,55 +13,8 @@ import (
 
 //go:generate go run github.com/99designs/gqlgen generate
 
-var (
-	port = "8080"
-	pMu  sync.Mutex
-
-	corsOpts = cors.Options{
-		AllowedOrigins: []string{"https://*", "http://*"},
-		AllowedMethods: []string{"GET", "POST", "OPTIONS"},
-		AllowedHeaders: []string{
-			"Content-Type", "Accept", "Authorization",
-			"X-Amz-Date", "X-Api-Key", "X-Amz-Security-Token", "X-Amz-User-Agent",
-		},
-	}
-)
-
-func SetPort(addr string) {
-	pMu.Lock()
-	defer pMu.Unlock()
-	port = addr
-}
-
 type Resolver struct {
 	weaver.Implements[weaver.Main]
 	CfpService  weaver.Ref[cfp.Service]
 	DkUiService weaver.Ref[dkui.Service]
-}
-
-func Serve(ctx context.Context, r *Resolver) error {
-	router := chi.NewRouter()
-	router.Use(gm.ClientIP)
-	router.Use(cors.Handler(corsOpts))
-
-	srv := handler.NewDefaultServer(NewExecutableSchema(Config{
-		Resolvers: r,
-	}))
-
-	opts := weaver.ListenerOptions{LocalAddress: ":" + port}
-	lis, err := r.Listener("dkw-serve", opts)
-	if err != nil {
-		return err
-	}
-	log.Printf("Listener available on %v\n", lis)
-
-	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	router.Handle("/query", srv)
-
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	s := http.Server{
-		ReadHeaderTimeout: 5 * time.Second,
-		Handler:           router,
-	}
-	return s.Serve(lis)
 }
