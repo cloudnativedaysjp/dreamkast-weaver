fe0142931241aa30233bba382878c1ac3ea2e0c7
ystkfujii <ystk.fujii0731@gmail.com>
2025-02-16 12:51:37 +0000

    refactor: Integrate db for dkui and cfp

--- internal/infrastructure/mysql/query.sql
+++ internal/infrastructure/mysql/query.sql
@@ -41,3 +41,20 @@ FROM
   track_viewer
 WHERE
   created_at BETWEEN ? AND ?;
+
+-- name: ListCfpVotes :many
+SELECT * FROM cfp_votes
+WHERE
+  conference_name = ? AND
+  created_at > sqlc.arg(start) AND
+  created_at < sqlc.arg(end);
+
+-- name: InsertCfpVote :exec
+INSERT INTO cfp_votes (
+  conference_name,
+  talk_id,
+  client_ip,
+  created_at
+) VALUES (
+  ?, ?, ?, now()
+);
