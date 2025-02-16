22c9a1ae8aeb41b41da22183469ac4ed7f51b51a
ystkfujii <ystk.fujii0731@gmail.com>
2025-02-16 12:51:37 +0000

    refactor: Integrate db for dkui and cfp

--- internal/infrastructure/mysql/repo/repo.go
+++ internal/infrastructure/mysql/repo/repo.go
@@ -295,3 +295,66 @@ func (_trackViewerConv) fromDB(tvs []TrackViewer) (*domain.TrackViewers, error) 
 // 	}
 // 	return tvs
 // }
+
+func (r *DkUiRepoImpl) ListCfpVotes(ctx context.Context, confName value.ConfName, vt value.VotingTerm) (*domain.CfpVotes, error) {
+	s, e := vt.Value()
+	req := ListCfpVotesParams{
+		ConferenceName: string(confName.Value()),
+		Start:          s,
+		End:            e,
+	}
+
+	votes, err := r.q.ListCfpVotes(ctx, req)
+	if err != nil {
+		return nil, stacktrace.With(fmt.Errorf("list cfp vote: %w", err))
+	}
+
+	return cfpVoteConv.fromDB(votes)
+}
+
+func (r *DkUiRepoImpl) InsertCfpVote(ctx context.Context, confName value.ConfName, talkID value.TalkID, clientIp net.IP) error {
+	req := InsertCfpVoteParams{
+		ConferenceName: string(confName.Value()),
+		TalkID:         talkID.Value(),
+		ClientIp: sql.NullString{
+			String: clientIp.String(),
+			Valid:  true,
+		},
+	}
+
+	if err := r.q.InsertCfpVote(ctx, req); err != nil {
+		return stacktrace.With(fmt.Errorf("incert cfp vote: %w", err))
+	}
+	return nil
+}
+
+var cfpVoteConv _cfpVoteConv
+
+type _cfpVoteConv struct{}
+
+func (_cfpVoteConv) fromDB(v []CfpVote) (*domain.CfpVotes, error) {
+	conv := func(v *CfpVote) (*domain.CfpVote, error) {
+		talkID, err := value.NewTalkID(v.TalkID)
+		if err != nil {
+			return nil, err
+		}
+		ip := net.ParseIP(v.ClientIp.String)
+		return &domain.CfpVote{
+			TalkID:    talkID,
+			ClientIp:  ip,
+			CreatedAt: v.CreatedAt,
+		}, nil
+	}
+
+	var items []domain.CfpVote
+	for _, p := range v {
+		cv := p
+		dcv, err := conv(&cv)
+		if err != nil {
+			return nil, stacktrace.With(fmt.Errorf("convert view event from DB: %w", err))
+		}
+		items = append(items, *dcv)
+	}
+
+	return &domain.CfpVotes{Items: items}, nil
+}
