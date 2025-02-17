9e4e51cc2c1723db7cbeca035fe5903446cbaa84
ystkfujii <ystk.fujii0731@gmail.com>
2025-02-17 14:33:43 +0000

    fix: Adjustments under usecase

--- internal/usecase/cfp.go
+++ internal/usecase/cfp.go
@@ -1,96 +1,64 @@
-package cfp
+package usecase
 
 import (
 	"context"
 	"net"
 
-	"dreamkast-weaver/internal/cfp/domain"
-	"dreamkast-weaver/internal/cfp/repo"
-	"dreamkast-weaver/internal/cfp/value"
 	"dreamkast-weaver/internal/derrors"
+	"dreamkast-weaver/internal/domain"
+	"dreamkast-weaver/internal/infrastructure/db/repo"
+	"dreamkast-weaver/internal/logger"
 	"dreamkast-weaver/internal/sqlhelper"
 	"dreamkast-weaver/internal/stacktrace"
-
-	"github.com/ServiceWeaver/weaver"
+	"dreamkast-weaver/internal/value"
 )
 
-type Service interface {
+type CfpService interface {
 	Vote(ctx context.Context, req VoteRequest) error
 	VoteCounts(ctx context.Context, req VoteCountsRequest) ([]*domain.VoteCount, error)
 }
 
 type VoteRequest struct {
-	weaver.AutoMarshal
 	ConfName value.ConfName
 	TalkID   value.TalkID
 	ClientIp net.IP
 }
 
 type VoteCountsRequest struct {
-	weaver.AutoMarshal
 	ConfName    value.ConfName
 	VotingTerm  value.VotingTerm
 	SpanSeconds value.SpanSeconds
 }
 
-type ServiceImpl struct {
-	weaver.Implements[Service]
-	weaver.WithConfig[config]
-
+type CfpServiceImpl struct {
 	sh     *sqlhelper.SqlHelper
 	domain domain.CfpDomain
 }
 
-var _ Service = (*ServiceImpl)(nil)
+var _ CfpService = (*CfpServiceImpl)(nil)
 
-type config struct {
-	DBUser     string `toml:"db_user"`
-	DBPassword string `toml:"db_password"`
-	DBEndpoint string `toml:"db_endpoint"`
-	DBPort     string `toml:"db_port"`
-	DBName     string `toml:"db_name"`
+func NewCFPService(sh *sqlhelper.SqlHelper) CfpService {
+	return &CfpServiceImpl{sh: sh}
 }
 
-func (c *config) SqlOption() *sqlhelper.SqlOption {
-	return &sqlhelper.SqlOption{
-		User:     c.DBUser,
-		Password: c.DBPassword,
-		Endpoint: c.DBEndpoint,
-		Port:     c.DBPort,
-		DbName:   c.DBName,
-	}
-}
-
-func NewService(sh *sqlhelper.SqlHelper) Service {
-	return &ServiceImpl{sh: sh}
-}
-
-func (s *ServiceImpl) Init(ctx context.Context) error {
-	opt := s.Config().SqlOption()
-	if err := opt.Validate(); err != nil {
-		opt = sqlhelper.NewOptionFromEnv("cfp")
-	}
-	sh, err := sqlhelper.NewSqlHelper(opt)
-	if err != nil {
-		return err
-	}
-	s.sh = sh
+func (s *CfpServiceImpl) Init(ctx context.Context) error {
 	return nil
 }
 
-func (s *ServiceImpl) HandleError(msg string, err error) {
+func (s *CfpServiceImpl) HandleError(ctx context.Context, msg string, err error) {
+	logger := logger.FromCtx(ctx)
 	if err != nil {
 		if derrors.IsUserError(err) {
-			s.Logger().With("errorType", "user-side").Info(msg, err)
+			logger.With("errorType", "user-side").Info(msg, err)
 		} else {
-			s.Logger().With("stacktrace", stacktrace.Get(err)).Error(msg, err)
+			logger.With("stacktrace", stacktrace.Get(err)).Error(msg, err)
 		}
 	}
 }
 
-func (s *ServiceImpl) VoteCounts(ctx context.Context, req VoteCountsRequest) (resp []*domain.VoteCount, err error) {
+func (s *CfpServiceImpl) VoteCounts(ctx context.Context, req VoteCountsRequest) (resp []*domain.VoteCount, err error) {
 	defer func() {
-		s.HandleError("get voteCounts", err)
+		s.HandleError(ctx, "get voteCounts", err)
 	}()
 
 	r := repo.NewCfpRepo(s.sh.DB())
@@ -105,9 +73,9 @@ func (s *ServiceImpl) VoteCounts(ctx context.Context, req VoteCountsRequest) (re
 	return dvc, nil
 }
 
-func (s *ServiceImpl) Vote(ctx context.Context, req VoteRequest) (err error) {
+func (s *CfpServiceImpl) Vote(ctx context.Context, req VoteRequest) (err error) {
 	defer func() {
-		s.HandleError("vote", err)
+		s.HandleError(ctx, "vote", err)
 	}()
 
 	r := repo.NewCfpRepo(s.sh.DB())
--- internal/usecase/cfp_test.go
+++ internal/usecase/cfp_test.go
@@ -1,93 +1,61 @@
-package cfp_test
+package usecase
 
 import (
 	"context"
 	"net"
-	"net/url"
 	"testing"
 	"time"
 
-	"dreamkast-weaver/internal/cfp"
-	"dreamkast-weaver/internal/cfp/value"
+	"dreamkast-weaver/internal/sqlhelper"
+	"dreamkast-weaver/internal/value"
 
-	"github.com/ServiceWeaver/weaver/weavertest"
-	"github.com/amacneil/dbmate/v2/pkg/dbmate"
 	_ "github.com/amacneil/dbmate/v2/pkg/driver/mysql"
 	"github.com/stretchr/testify/assert"
 )
 
-const (
-	weaverConfig = `
-	["dreamkast-weaver/internal/cfp/Service"]
-	db_user = "user"
-	db_password = "password"
-	db_endpoint = "127.0.0.1"
-	db_port = "13306"
-	db_name = "test_cfp"
-	`
-	dbUrl = "mysql://user:password@127.0.0.1:13306/test_cfp"
-)
-
-func TestMain(m *testing.M) {
-	setup()
-	defer teardown()
-	m.Run()
-}
-
-func setup() {
-	u, _ := url.Parse(dbUrl)
-	db := dbmate.New(u)
-	db.AutoDumpSchema = false
-
-	mustNil(db.Drop())
-	mustNil(db.CreateAndMigrate())
-}
-
-func teardown() {}
-
 func TestCfpVoteImpl(t *testing.T) {
 	t.Skip()
 
-	runner := weavertest.Local
-	runner.Config = weaverConfig
-	runner.Test(t, func(t *testing.T, svc cfp.Service) {
-		ctx := context.Background()
-		cn := value.CICD2023
-		talkID, _ := value.NewTalkID(3)
+	ctx := context.Background()
+	cn := value.CICD2023
+	talkID, _ := value.NewTalkID(3)
 
-		err := svc.Vote(ctx, cfp.VoteRequest{
-			ConfName: cn,
-			TalkID:   talkID,
-			ClientIp: net.ParseIP("127.0.0.1"),
-		})
-		assert.NoError(t, err)
-
-		vts := time.Now().AddDate(0, 0, -1)
-		vte := time.Now().AddDate(0, 0, 1)
-		vt, _ := value.NewVotingTerm(&vts, &vte)
-
-		resp, err := svc.VoteCounts(ctx, cfp.VoteCountsRequest{
-			ConfName:    cn,
-			VotingTerm:  vt,
-			SpanSeconds: newSpanSeconds(value.SPAN_SECONDS),
-		})
-		assert.NoError(t, err)
-
-		var ok bool
-		for _, r := range resp {
-			if r.TalkID == talkID {
-				ok = true
-				assert.Greater(t, r.Count, 0)
-			}
-		}
-		assert.True(t, ok, "talkID not found")
+	sq, _ := sqlhelper.NewSqlHelper(&sqlhelper.SqlOption{
+		User:     "user",
+		Password: "password",
+		Endpoint: "127.0.0.1",
+		Port:     "13306",
+		DbName:   "test_cfp",
 	})
-}
 
-func mustNil(err error) {
-	if err != nil {
-		panic(err)
+	svc := NewCFPService(sq)
+
+	err := svc.Vote(ctx, VoteRequest{
+		ConfName: cn,
+		TalkID:   talkID,
+		ClientIp: net.ParseIP("127.0.0.1"),
+	})
+	assert.NoError(t, err)
+
+	vts := time.Now().AddDate(0, 0, -1)
+	vte := time.Now().AddDate(0, 0, 1)
+	vt, _ := value.NewVotingTerm(&vts, &vte)
+
+	resp, err := svc.VoteCounts(ctx, VoteCountsRequest{
+		ConfName:    cn,
+		VotingTerm:  vt,
+		SpanSeconds: newSpanSeconds(value.SPAN_SECONDS),
+	})
+	assert.NoError(t, err)
+
+	var ok bool
+	for _, r := range resp {
+		if r.TalkID == talkID {
+			ok = true
+			assert.Greater(t, r.Count, 0)
+		}
 	}
+	assert.True(t, ok, "talkID not found")
 }
 
 func newSpanSeconds(v int) value.SpanSeconds {
