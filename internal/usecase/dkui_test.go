bccd11e434156b748e5ef1e9c53099ba38f4aef0
ystkfujii <ystk.fujii0731@gmail.com>
2025-02-17 14:33:43 +0000

    fix: Adjustments under usecase

--- internal/usecase/dkui_test.go
+++ internal/usecase/dkui_test.go
@@ -1,30 +1,21 @@
-package dkui_test
+package usecase
 
 import (
 	"context"
 	"net/url"
 	"testing"
 
-	"dreamkast-weaver/internal/dkui"
-	"dreamkast-weaver/internal/dkui/domain"
-	"dreamkast-weaver/internal/dkui/value"
+	"dreamkast-weaver/internal/domain"
+	"dreamkast-weaver/internal/sqlhelper"
+	"dreamkast-weaver/internal/value"
 
-	"github.com/ServiceWeaver/weaver/weavertest"
 	"github.com/amacneil/dbmate/v2/pkg/dbmate"
 	_ "github.com/amacneil/dbmate/v2/pkg/driver/mysql"
 	"github.com/stretchr/testify/assert"
 )
 
 const (
-	weaverConfig = `
-	["dreamkast-weaver/internal/dkui/Service"]
-	db_user = "user"
-	db_password = "password"
-	db_endpoint = "127.0.0.1"
-	db_port = "13306"
-	db_name = "test_dkui"
-	`
-	dbUrl = "mysql://user:password@127.0.0.1:13306/test_dkui"
+	dkuiDBUrl = "mysql://user:password@127.0.0.1:13306/test_dkui"
 )
 
 func TestMain(m *testing.M) {
@@ -34,8 +25,8 @@ func TestMain(m *testing.M) {
 }
 
 func setup() {
-	u, _ := url.Parse(dbUrl)
-	db := dbmate.New(u)
+	uu, _ := url.Parse(dkuiDBUrl)
+	db := dbmate.New(uu)
 	db.AutoDumpSchema = false
 
 	mustNil(db.Drop())
@@ -48,125 +39,142 @@ func TestDkUiServiceImpl_CreateViewEvent(t *testing.T) {
 	domain.ChangeGuardSecondsForTest(0)
 	domain.ChangeStampReadySecondsForTest(value.INTERVAL_SECONDS * 2)
 
-	runner := weavertest.Local
-	runner.Config = weaverConfig
-	runner.Test(t, func(t *testing.T, svc dkui.Service) {
-		ctx := context.Background()
+	ctx := context.Background()
 
-		profile := dkui.Profile{
-			ConfName: newConfName("cndt2023"),
-			ID:       newProfileID(1),
-		}
-		req := dkui.CreateViewEventRequest{
-			TrackID: newTrackID(2),
-			TalkID:  newTalkID(3),
-			SlotID:  newSlotID(1000),
-		}
-
-		// first time
-		err := svc.CreateViewEvent(ctx, profile, req)
-		assert.NoError(t, err)
-
-		events, err := svc.ViewingEvents(ctx, profile)
-		assert.NoError(t, err)
-
-		stamps, err := svc.StampChallenges(ctx, profile)
-		assert.NoError(t, err)
-
-		assertViewEvents(t, events, 1000, value.INTERVAL_SECONDS)
-		assert.Len(t, stamps.Items, 0)
-
-		// second time
-		err = svc.CreateViewEvent(ctx, profile, req)
-		assert.NoError(t, err)
-
-		events, err = svc.ViewingEvents(ctx, profile)
-		assert.NoError(t, err)
-
-		stamps, err = svc.StampChallenges(ctx, profile)
-		assert.NoError(t, err)
-
-		assertViewEvents(t, events, 1000, value.INTERVAL_SECONDS*2)
-		assert.Len(t, stamps.Items, 1)
-		assertStampCondition(t, stamps, 1000, "ready")
-
-		// stamp
-		err = svc.StampOnline(ctx, profile, req.SlotID)
-		assert.NoError(t, err)
-
-		events, err = svc.ViewingEvents(ctx, profile)
-		assert.NoError(t, err)
-
-		stamps, err = svc.StampChallenges(ctx, profile)
-		assert.NoError(t, err)
-
-		assertViewEvents(t, events, 1000, value.INTERVAL_SECONDS*2)
-		assert.Len(t, stamps.Items, 1)
-		assertStampCondition(t, stamps, 1000, "stamped")
+	sq, _ := sqlhelper.NewSqlHelper(&sqlhelper.SqlOption{
+		User:     "user",
+		Password: "password",
+		Endpoint: "127.0.0.1",
+		Port:     "13306",
+		DbName:   "test_dkui",
 	})
+
+	svc := NewDkUiService(sq)
+
+	profile := Profile{
+		ConfName: newConfName("cndt2023"),
+		ID:       newProfileID(1),
+	}
+	req := CreateViewEventRequest{
+		TrackID: newTrackID(2),
+		TalkID:  newTalkID(3),
+		SlotID:  newSlotID(1000),
+	}
+
+	// first time
+	err := svc.CreateViewEvent(ctx, profile, req)
+	assert.NoError(t, err)
+
+	events, err := svc.ViewingEvents(ctx, profile)
+	assert.NoError(t, err)
+
+	stamps, err := svc.StampChallenges(ctx, profile)
+	assert.NoError(t, err)
+
+	assertViewEvents(t, events, 1000, value.INTERVAL_SECONDS)
+	assert.Len(t, stamps.Items, 0)
+
+	// second time
+	err = svc.CreateViewEvent(ctx, profile, req)
+	assert.NoError(t, err)
+
+	events, err = svc.ViewingEvents(ctx, profile)
+	assert.NoError(t, err)
+
+	stamps, err = svc.StampChallenges(ctx, profile)
+	assert.NoError(t, err)
+
+	assertViewEvents(t, events, 1000, value.INTERVAL_SECONDS*2)
+	assert.Len(t, stamps.Items, 1)
+	assertStampCondition(t, stamps, 1000, "ready")
+
+	// stamp
+	err = svc.StampOnline(ctx, profile, req.SlotID)
+	assert.NoError(t, err)
+
+	events, err = svc.ViewingEvents(ctx, profile)
+	assert.NoError(t, err)
+
+	stamps, err = svc.StampChallenges(ctx, profile)
+	assert.NoError(t, err)
+
+	assertViewEvents(t, events, 1000, value.INTERVAL_SECONDS*2)
+	assert.Len(t, stamps.Items, 1)
+	assertStampCondition(t, stamps, 1000, "stamped")
 }
 
 func TestDkUiServiceImpl_StampOnSite(t *testing.T) {
-	runner := weavertest.Local
-	runner.Config = weaverConfig
-	runner.Test(t, func(t *testing.T, svc dkui.Service) {
-		ctx := context.Background()
 
-		profile := dkui.Profile{
-			ConfName: newConfName("cndt2023"),
-			ID:       newProfileID(1),
-		}
-		req := dkui.StampRequest{
-			TrackID: newTrackID(2),
-			TalkID:  newTalkID(3),
-			SlotID:  newSlotID(1001),
-		}
-
-		err := svc.StampOnSite(ctx, profile, req)
-		assert.NoError(t, err)
-
-		slots, err := svc.ViewingEvents(ctx, profile)
-		assert.NoError(t, err)
-
-		stamps, err := svc.StampChallenges(ctx, profile)
-		assert.NoError(t, err)
-
-		assertViewEvents(t, slots, 1001, value.TALK_SECONDS)
-		assertStampCondition(t, stamps, 1001, "stamped")
+	ctx := context.Background()
+	sq, _ := sqlhelper.NewSqlHelper(&sqlhelper.SqlOption{
+		User:     "user",
+		Password: "password",
+		Endpoint: "127.0.0.1",
+		Port:     "13306",
+		DbName:   "test_dkui",
 	})
+
+	svc := NewDkUiService(sq)
+
+	profile := Profile{
+		ConfName: newConfName("cndt2023"),
+		ID:       newProfileID(1),
+	}
+	req := StampRequest{
+		TrackID: newTrackID(2),
+		TalkID:  newTalkID(3),
+		SlotID:  newSlotID(1001),
+	}
+
+	err := svc.StampOnSite(ctx, profile, req)
+	assert.NoError(t, err)
+
+	slots, err := svc.ViewingEvents(ctx, profile)
+	assert.NoError(t, err)
+
+	stamps, err := svc.StampChallenges(ctx, profile)
+	assert.NoError(t, err)
+
+	assertViewEvents(t, slots, 1001, value.TALK_SECONDS)
+	assertStampCondition(t, stamps, 1001, "stamped")
 }
 
 func TestDkUiServiceImpl_ListTrackViewer(t *testing.T) {
-	runner := weavertest.Local
-	runner.Config = weaverConfig
-	runner.Test(t, func(t *testing.T, svc dkui.Service) {
-		ctx := context.Background()
-
-		tna := newTrackName("A")
-		tnb := newTrackName("B")
-		tnc := newTrackName("C")
-		tIDa := newTalkID(1)
-		tIDb := newTalkID(2)
-		tIDc := newTalkID(3)
-
-		assert.NoError(t, svc.ViewTrack(ctx, newProfileID(731), tna, tIDa))
-		assert.NoError(t, svc.ViewTrack(ctx, newProfileID(731), tna, tIDa))
-		assert.NoError(t, svc.ViewTrack(ctx, newProfileID(732), tnb, tIDb))
-		assert.NoError(t, svc.ViewTrack(ctx, newProfileID(733), tnc, tIDb))
-		assert.NoError(t, svc.ViewTrack(ctx, newProfileID(734), tnc, tIDc))
-
-		ans := map[value.TrackName]int{}
-		ans[tna] = 1
-		ans[tnb] = 1
-		ans[tnc] = 2
-
-		dvc, err := svc.ListViewerCounts(ctx, false)
-		assert.NoError(t, err)
-
-		for _, v := range dvc.Items {
-			assert.Equal(t, ans[v.TrackName], v.Count)
-		}
+	ctx := context.Background()
+	sq, _ := sqlhelper.NewSqlHelper(&sqlhelper.SqlOption{
+		User:     "user",
+		Password: "password",
+		Endpoint: "127.0.0.1",
+		Port:     "13306",
+		DbName:   "test_dkui",
 	})
+
+	svc := NewDkUiService(sq)
+
+	tna := newTrackName("A")
+	tnb := newTrackName("B")
+	tnc := newTrackName("C")
+	tIDa := newTalkID(1)
+	tIDb := newTalkID(2)
+	tIDc := newTalkID(3)
+
+	assert.NoError(t, svc.ViewTrack(ctx, newProfileID(731), tna, tIDa))
+	assert.NoError(t, svc.ViewTrack(ctx, newProfileID(731), tna, tIDa))
+	assert.NoError(t, svc.ViewTrack(ctx, newProfileID(732), tnb, tIDb))
+	assert.NoError(t, svc.ViewTrack(ctx, newProfileID(733), tnc, tIDb))
+	assert.NoError(t, svc.ViewTrack(ctx, newProfileID(734), tnc, tIDc))
+
+	ans := map[value.TrackName]int{}
+	ans[tna] = 1
+	ans[tnb] = 1
+	ans[tnc] = 2
+
+	dvc, err := svc.ListViewerCounts(ctx, false)
+	assert.NoError(t, err)
+
+	for _, v := range dvc.Items {
+		assert.Equal(t, ans[v.TrackName], v.Count)
+	}
 }
 
 func mustNil(err error) {
@@ -198,8 +206,8 @@ func assertViewEvents(t *testing.T, events *domain.ViewEvents, slotID int32, vt 
 	assert.Equal(t, vt, total)
 }
 
-func newProfile(confName value.ConferenceKind, profile int32) dkui.Profile {
-	return dkui.Profile{
+func newProfile(confName value.ConferenceKind, profile int32) Profile {
+	return Profile{
 		ConfName: newConfName(confName),
 		ID:       newProfileID(profile),
 	}
